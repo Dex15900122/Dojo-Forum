dfc6d7e5da644554067b389fba2825d39819288f
Dex15900122 <e5900122@yahoo.com.tw>
2018-04-22 10:55:28 +0800

    add articles index controllers and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do |article|
+  Article.create!(
+    title: "Post title",
+    replies_count: rand(00..99),
+    viewed_count: rand(00..99)
+  )
+end
